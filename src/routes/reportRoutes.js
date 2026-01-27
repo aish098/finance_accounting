9248f92fbf37694d275ea883564edee9664ea2d0
aish098 <ak0796587@gmail.com>
2026-01-28 00:28:48 +0500

    "Implement_General_Ledger_UI_and_Route"

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -5,5 +5,6 @@ const reportController = require('../controllers/reportController');
 router.get('/trial-balance', (req, res) => reportController.getTrialBalance(req, res));
 router.get('/profit-loss', (req, res) => reportController.getProfitAndLoss(req, res));
 router.get('/balance-sheet', (req, res) => reportController.getBalanceSheet(req, res));
+router.get('/ledger', (req, res) => reportController.getLedger(req, res));
 
 module.exports = router;
